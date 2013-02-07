b64d0eb63a863dab3dd4e39ed9db47816f3159fa
zoetrope <a.ikezoe@gmail.com>
2013-02-07 22:37:07 +0900

    try BSON

--- ReactiveRTM.Test/SimpleServiceTest.cs
+++ ReactiveRTM.Test/SimpleServiceTest.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ReactiveRTM.Core;
+using Newtonsoft.Json.Bson;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace ReactiveRTM.Test
 {
@@ -24,6 +28,34 @@ namespace ReactiveRTM.Test
 
             double ret = cons.Test(10);
             ret.Is(100);
+        }
+
+        [TestMethod]
+        public void BSONがつかえるかどうか()
+        {
+            
+            var js = new JsonSerializer();
+
+            var ms = new MemoryStream();
+            var bw = new BsonWriter(ms);
+
+            js.Serialize(bw, new { Name = "hoge", Age = 13 });
+
+            Console.WriteLine(BitConverter.ToString(ms.ToArray()));
+
+
+            ms.Seek(0, SeekOrigin.Begin);
+
+
+
+            // deserialize product from BSON
+
+            var br = new BsonReader(ms);
+
+            dynamic ret = js.Deserialize(br);
+
+            Console.WriteLine(ret.Name);
+            Console.WriteLine(ret.Age);
         }
 
 
